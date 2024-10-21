08f2632167a65c11bcd0c802c49a5d20015d6f14
Maciek <maciek.krzyzowski44@gmail.com>
2024-10-21 19:19:24 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <main className="flex min-h-screen items-center justify-between bg-slate-50 py-10">
-      <div className="container mx-auto px-4">strona startowa</div>
+      <div className="container mx-auto px-4">strona startowa 1</div>
     </main>
   );
 };
